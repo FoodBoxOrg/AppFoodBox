2d5f4b3d8187de198d0cee908656a264a84b976d
chiyeb <chiheb.bradai@etu.univ-amu.fr>
2025-01-30 14:27:55 +0100

    [Ajout] Ajout des repositories

--- src/Repository/FoodRepository.php
+++ src/Repository/FoodRepository.php
@@ -2,39 +2,39 @@
 
 namespace App\Repository;
 
-use App\Entity\User;
+use App\Entity\Food;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<User>
+ * @extends ServiceEntityRepository<Food>
  */
-class UserRepository extends ServiceEntityRepository
+class FoodRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, User::class);
+        parent::__construct($registry, Food::class);
     }
 
     //    /**
-    //     * @return User[] Returns an array of User objects
+    //     * @return Food[] Returns an array of Food objects
     //     */
     //    public function findByExampleField($value): array
     //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
+    //        return $this->createQueryBuilder('f')
+    //            ->andWhere('f.exampleField = :val')
     //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
+    //            ->orderBy('f.id', 'ASC')
     //            ->setMaxResults(10)
     //            ->getQuery()
     //            ->getResult()
     //        ;
     //    }
 
-    //    public function findOneBySomeField($value): ?User
+    //    public function findOneBySomeField($value): ?Food
     //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
+    //        return $this->createQueryBuilder('f')
+    //            ->andWhere('f.exampleField = :val')
     //            ->setParameter('val', $value)
     //            ->getQuery()
     //            ->getOneOrNullResult()
